e817afa2574a89715c2fd95daf3972d5e031cb2e
skyyharris <skyy.harris@inspire35.org>
2025-11-21 21:22:37 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -4,6 +4,7 @@ $(function () {
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
 
+
   function setup() {
     if (firstTimeSetup) {
       halleImage = document.getElementById("player");
@@ -16,39 +17,60 @@ $(function () {
       setInterval(main, 1000 / frameRate);
     }
 
+
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
+
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
+
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
+
+
 
 
     // TODO 2 - Create Platforms
 
 
+    createPlatform(200, 650, 50, 20, "yellow");
+    createPlatform(300, 700, 160, 60, "blue");
+    createPlatform(400, 600, 170, 80, "pink");
+    createPlatform(500, 500, 180, 100, "purple");
+    createPlatform(600, 400,190, 120, "green");
+
+
 
 
     // TODO 3 - Create Collectables
+    createCollectable("steve", 300, 500);
+    createCollectable("kennedi", 500,400);
+    createCollectable("diamond", 600, 300);
 
 
 
-    
+
+   
     // TODO 4 - Create Cannons
+    createCannon("right", 100, 700);
+    createCannon("top", 700, 500);
+    createCannon("bottom", 800, 900);
 
 
-    
-    
+   
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
   }
 
+
   registerSetup(setup);
 });
+
+
