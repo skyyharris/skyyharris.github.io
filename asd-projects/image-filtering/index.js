bcd2aa636e5a386e33a7e84137681afbedbfad8d
skyyharris <skyy.harris@inspire35.org>
2026-05-04 19:33:08 +0000

    message

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,9 +1,9 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads.
 $(document).ready(function () {
-  render($("#display"), image);
-  $("#apply").on("click", applyAndRender);
-  $("#reset").on("click", resetAndRender);
+render($("#display"), image);
+$("#apply").on("click", applyAndRender);
+$("#reset").on("click", resetAndRender);
 });
 
 /////////////////////////////////////////////////////////
@@ -12,19 +12,21 @@ $(document).ready(function () {
 
 // this function resets the image to its original value; do not change this function
 function resetAndRender() {
-  reset();
-  render($("#display"), image);
+reset();
+render($("#display"), image);
 }
 
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
 function applyAndRender() {
-  // Multiple TODOs: Call your apply function(s) here
+// Multiple TODOs: Call your apply function(s) here
+applyFilter(reddify);
+applyFilterNoBackground(decreaseBlue);
+applyFilter(increaseGreenByBlue);
+applyFilterNoBackground(reddify);
 
-  
-
-  // do not change the below line of code
-  render($("#display"), image);
+// do not change the below line of code
+render($("#display"), image);
 }
 
 /////////////////////////////////////////////////////////
@@ -32,18 +34,51 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
+function applyFilter(filterFunction) {
+for (var i = 0; i < image.length; i++) {
+for (var j = 0; j < image[i].length; j++) {
+var pixel = image[i][j];
+var pixelArray = rgbStringToArray(pixel);
 
+filterFunction(pixelArray);
+
+var updatedPixel = rgbArrayToString(pixelArray);
+image[i][j] = updatedPixel;
+}
+}
+}
 
 // TODO 9 Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) { // Added { here
+var backgroundColor = image[0][0];
+for (var i = 0; i < image.length; i++) {
+for (var j = 0; j < image[i].length; j++) {
+if (image[i][j] !== backgroundColor) {
+var pixelArray = rgbStringToArray(image[i][j]);
+filterFunction(pixelArray);
+image[i][j] = rgbArrayToString(pixelArray);
+}
+}
+}
+} // Make sure this closing brace is at the end of the function!
 
 
 // TODO 6: Create the keepInBounds function
-
+function keepInBounds(num) {
+return num < 0 ? 0 : (num > 255 ? 255 : num);
+}
 
 // TODO 4: Create reddify filter function
-
+function reddify(pixelArray) {
+pixelArray[RED] = 200;
+}
 
 // TODO 7 & 8: Create more filter functions
+function decreaseBlue(pixelArray) {
+var newBlue = pixelArray[BLUE] - 50;
+pixelArray[BLUE] = keepInBounds(newBlue);
+}
 
-
-// CHALLENGE code goes below here
+function increaseGreenByBlue(pixelArray) {
+var newGreen = pixelArray[GREEN] + pixelArray[BLUE];
+pixelArray[GREEN] = keepInBounds(newGreen);
